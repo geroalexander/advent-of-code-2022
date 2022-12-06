2de91515d2a71e93568ef46c15e0deca65b9d916
Gero Kassing <gero.a.kassing@gmail.com>
2022-12-06 10:56:46 +0100

    feat: update function name

--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,5 @@
 import fs from "fs";
 
-export const read = (day: number) => {
+export const readInput = (day: number) => {
   return fs.readFileSync(`${__dirname}/inputs/day${day}.txt`, "utf-8");
 };
