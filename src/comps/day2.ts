047c63b0c4458c3223a3abf54eb690306c5d08f3
Gero Kassing <gero.a.kassing@gmail.com>
2022-12-11 11:28:04 +0100

    feat(day2): part 1 done

--- src/comps/day2.ts
+++ src/comps/day2.ts
@@ -1,10 +1,91 @@
 import { readInput } from "./../utils";
 
 export const calculateDay2 = () => {
-  console.log("Day 2:");
-  partOne();
+  console.log("Day 2:\n");
+  console.log(partOne());
+};
+
+const rawInput = readInput(2)
+  .split("\n")
+  .map((el) => el.split(" "));
+
+interface GameMap {
+  A: number;
+  B: number;
+  C: number;
+  X: number;
+  Y: number;
+  Z: number;
+}
+
+const gameMap: GameMap = {
+  A: 1,
+  B: 2,
+  C: 3,
+  X: 1,
+  Y: 2,
+  Z: 3,
+};
+
+const modifyRawData = (arr: Array<string[]>): Array<Array<number>> => {
+  return arr.map((el) => {
+    return el.map((str) => gameMap[str as keyof GameMap]);
+  });
 };
 
 const partOne = () => {
-  console.log("hi");
+  const modifedData = modifyRawData(rawInput);
+  let myScore = 0;
+
+  modifedData.forEach(([opponentHand, myHand]) => {
+    // Player chooses rock - 1
+    if (opponentHand === 1) {
+      // Tie (I choose rock - 1)
+      if (myHand === 1) {
+        myScore += myHand + 3;
+      }
+      // Opponent Win (I choose scissor - 3)
+      if (myHand === 3) {
+        myScore += myHand;
+      }
+      // Opponent Loss (I choose paper - 2)
+      if (myHand === 2) {
+        myScore += myHand + 6;
+      }
+    }
+
+    // Player chooses paper - 2
+    if (opponentHand === 2) {
+      // Tie (I choose paper - 2)
+      if (myHand === 2) {
+        myScore += myHand + 3;
+      }
+      // Opponent Win (I choose rock - 1)
+      if (myHand === 1) {
+        myScore += myHand;
+      }
+      // Opponent Loss (I choose scissor - 3)
+      if (myHand === 3) {
+        myScore += myHand + 6;
+      }
+    }
+
+    // Player chooses scissor - 3
+    if (opponentHand === 3) {
+      // Tie (I choose scissor - 3)
+      if (myHand === 3) {
+        myScore += myHand + 3;
+      }
+      // Opponent Win (I choose paper - 2)
+      if (myHand === 2) {
+        myScore += myHand;
+      }
+      // Opponent Loss (I choose rock - 1)
+      if (myHand === 1) {
+        myScore += myHand + 6;
+      }
+    }
+  });
+
+  return myScore;
 };
