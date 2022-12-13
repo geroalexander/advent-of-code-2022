85ca1c6c499486999f5e0f2eb948e2bfbf8db51d
Gero Kassing <gero.a.kassing@gmail.com>
2022-12-13 22:52:59 +0100

    chore: setup for day 3

---
+++ src/comps/day3.ts
@@ -0,0 +1,7 @@
+import { readInput } from "./../utils";
+
+export const calculateDay3 = () => {
+  console.log("Day 3:\n");
+  //   console.log(partOne());
+  //   console.log(partTwo());
+};
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import { calculateDay2 } from "./comps/day2";
 import { calculateDay1 } from "./comps/day1";
+import { calculateDay3 } from "./comps/day3";
 
 // calculateDay1();
-calculateDay2();
+// calculateDay2();
+calculateDay3();
